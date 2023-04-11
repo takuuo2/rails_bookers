9754a2d6d5301e76bee4330adf7ca998f5b6cefb
takuuo2 <126545872+takuuo2@users.noreply.github.com>
2023-04-11 14:07:53 +0000

    [ADD] delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#new'
-  get 'books/edit'
   get '/' => 'homes#top'
-  get 'books/:id' => 'books#show', as: 'book'
+  resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
