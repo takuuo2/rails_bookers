7596dea641c4e75c01b91ac8b5ddd27c8e2833c7
takuuo2 <126545872+takuuo2@users.noreply.github.com>
2023-04-12 13:56:07 +0000

    preFinish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-
-    book.save
-
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      return redirect_to book_path(@book.id),notice:'Book was successfully created.'
+    else
+      render "index"
+    end
   end
 
   def index
@@ -25,9 +27,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      return redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      render "edit"
+    end
+
   end
 
   def destroy
